ff763906677d071885718518b728eb450e51d3ba
chaosgao <498340538@qq.com>
2022-07-21 09:39:44 +0800

    springcloud集群

---
+++ org-exam-ribbon-manager/src/main/java/org/exam/OrgExamRibbonManagerApplication.java
@@ -0,0 +1,13 @@
+package org.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgExamRibbonManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgExamRibbonManagerApplication.class, args);
+    }
+
+}
